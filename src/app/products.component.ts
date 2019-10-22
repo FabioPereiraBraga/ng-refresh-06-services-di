2237891a8268ef686313afb3cca306dbf2c36f8d
Fabio Braga <fabio.braga@basis.com.br>
2019-10-22 16:54:29 -0300

    first commit

---
+++ src/app/products.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { ProductsService } from './products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent implements OnInit, OnDestroy {
+
+  products = [];
+  private productsSubscription: Subscription;
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+    this.productsSubscription = this.productsService.productsUpdated.subscribe(() => {
+      alert('teste');
+      this.products = this.productsService.getProducts();
+    });
+  }
+
+  onAddProduct(form) {
+    if (form.valid) {
+      this.productsService.addProduct(form.value.productName);
+    }
+  }
+  ngOnDestroy() {
+    this.productsSubscription.unsubscribe();
+  }
+}
